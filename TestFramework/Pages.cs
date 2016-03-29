57aa2e2229f7fbf9c5e86fd46f22dbd7fc74c342
rrrzzz <kalandrill@yahoo.com>
2016-03-29 16:23:04 +0300

    Rewrote HomePage, AuthorsPage, AuthorPage, Footer, Header classes as nonstatic. Added fields and properties to Pages class to contain their instances. Added implementation of PageObjectModel with PageFactory implementation in each non-static class.

--- TestFramework/Pages.cs
+++ TestFramework/Pages.cs
@@ -8,118 +8,192 @@ using OpenQA.Selenium.Support;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.PageObjects;
 
 namespace TestFramework
 {
-    public static class Page
+    public static class Pages
     {
-        public static class Header
+        static HomePage _homePage;
+        public static HomePage HomePage
         {
+            get
+            {
+                if (_homePage == null)
+                {
+                    _homePage = new HomePage();
+                }
+                return _homePage;
+            }
+        }
+
+        static Footer _footer;
+        public static Footer Footer
+        {
+            get
+            {
+                if (_footer == null)
+                {
+                    _footer= new Footer();
+                }
+                PageFactory.InitElements(Browser.Driver, _footer);
+                return _footer;
+            }
 
         }
-        
-        public static class Footer
-        {
-            public static void AuthorsClick()
-            {
-                Browser.driver.FindElement(By.LinkText("Лекторы")).Click();
-            }            
 
+        static AuthorsPage _authorsPage;
+        public static AuthorsPage AuthorsPage
+        {
+            get
+            {
+                if (_authorsPage == null)
+                {
+                    _authorsPage = new AuthorsPage();
+                }
+                PageFactory.InitElements(Browser.Driver, _authorsPage);
+                return _authorsPage;
+            }
+        }
+
+        static AuthorPage _author;
+        public static AuthorPage Author
+        {
+            get
+            {
+                if (_author == null)
+                {
+                    _author = new AuthorPage();
+                }
+                PageFactory.InitElements(Browser.Driver, _author);
+                return _author;
+            }
         }
 
     }
 
-    public static class HomePage
+    public class HomePage
     {
         static private string _url = "http://arzamas.academy";
         static private string _title = "Arzamas";
 
-        public static void GoTo()
+        public void GoTo()
         {
             Browser.GoTo(_url);
         }
 
-        public static bool IsAt()
+        public bool IsAt()
         {
-            return Browser.driver.Title == _title;
+            return Browser.Title == _title;
         }
-
         
     }
 
-    public static class AuthorsPage
+    public class AuthorsPage
     {
-        static private string _url = "http://arzamas.academy/authors";
-                
-        public static void GoTo()
+        static string _url = "http://arzamas.academy/authors";
+        IWebElement _authorLink;
+        string _linkName;
+        public string LinkName { get { return _linkName; } }
+
+        [FindsBySequence]
+        [FindsBy(How = How.ClassName, Using = "b-authors", Priority = 0)]
+        [FindsBy(How = How.TagName, Using = "li", Priority = 1)]
+        IList<IWebElement> _allAuthors;
+                     
+        public void GoTo()
         {
             Browser.GoTo(_url);
         }
 
-        public static void ClickAny()
+        public void ClickAny()
         {
             var rand = new Random();
-            IReadOnlyCollection<IWebElement> authors = GetAuthors();
-            Author.Current = authors.ElementAt(rand.Next(authors.Count));
-            //Author.Current = authors.ElementAt(3);
-            Author.Click();
+            //IReadOnlyCollection<IWebElement> authors = GetAuthors();
+            _authorLink = _allAuthors[rand.Next(_allAuthors.Count)];
+            _linkName = GetLinkName();
+            ClickAuthorLink();
+           
         }
 
-        public static bool CheckAllAuthors()
+        public bool CheckAllAuthors()
         {
-            int size = GetAuthors().Count; 
-            for (int i = 0; i < size; i++)
+            for (int i = 0; i < _allAuthors.Count; i++)
             {
-                var authors = GetAuthors();
-                Author.Current = authors.ElementAt(i);
-                Author.Click();
-                if (!Author.AtAuthorPage())
-                    throw new IncorrectAuthorException($"There is a mismatch on {Author.Name}'s page!");
-                Browser.driver.Navigate().Back();
+                //IReadOnlyCollection<IWebElement> authors = GetAuthors();
+                _authorLink = _allAuthors[i];
+                _linkName = GetLinkName();
+                ClickAuthorLink();
+                if (!Pages.Author.AtAuthorPage())
+                    throw new IncorrectAuthorException($"There is a mismatch on {Pages.Author.Name}'s page!");
+                Browser.Driver.Navigate().Back();
             }
             return true;
         }
 
-        private static IReadOnlyCollection<IWebElement> GetAuthors()
+        //private static IReadOnlyCollection<IWebElement> GetAuthors()
+        //{
+        //    var pack = Browser.Driver.FindElement(By.ClassName("b-authors"));
+        //    return pack.FindElements(By.TagName("li"));            
+        //}            
+
+        string GetLinkName()
         {
-            var pack = Browser.driver.FindElement(By.ClassName("b-authors"));
-            return pack.FindElements(By.TagName("li"));            
-        }            
+            return _authorLink.FindElement(By.TagName("h5")).Text;
+        }
+
+        void ClickAuthorLink()
+        {
+            Actions builder = new Actions(Browser.Driver);
+            builder.Click(_authorLink).Perform();
+        }        
 
     }
 
-    public static class Author
+    public class AuthorPage
     {
-        public static IWebElement Current;
-        public static string Name { get; set; }
+        [FindsBy(How = How.ClassName, Using = "author-name")]
+        IWebElement _nameElement;       
+        public string Name { get { return _nameElement.Text; } }
+        public string TitleName { get { return Browser.Title.Substring(0, Browser.Title.IndexOf('|') - 1); } }
 
-        public static bool AtAuthorPage()
-        {
-            var title = Browser.driver.Title;
-            title = title.Substring(0, title.IndexOf('|') - 1);            
-            return Browser.driver.FindElement(By.ClassName("author-name")).Text == Name && title == Name;
-        }
-
-        public static void Click()
-        {
-            Name = Current.FindElement(By.TagName("h5")).Text;
-            Actions builder = new Actions(Browser.driver);
-            builder.Click(Current).Perform();            
+        public bool AtAuthorPage()
+        {            
+            return Name == Pages.AuthorsPage.LinkName && TitleName == Name;
         }
     }
+
+    public class Header
+    {
+
+    }
+
+    public class Footer
+    {
+        [FindsBy(How = How.LinkText, Using = "Лекторы")]
+        IWebElement _authorsLink;
+
+        public void AuthorsClick()
+        {
+            _authorsLink.Click();
+        }
+    }
+
     public static class Browser
     {
-        public static IWebDriver driver = new ChromeDriver(@"C:\Libraries");
-        
+        static IWebDriver _driver = new ChromeDriver(@"C:\Libraries");
+        public static IWebDriver Driver { get { return _driver; } }
+        public static string Title { get { return _driver.Title; } }
+
         public static void GoTo(string Url)
         {
-            driver.Navigate().GoToUrl(Url);
+            _driver.Navigate().GoToUrl(Url);
         }
 
         public static void Close()
         {
-            driver.Close();
+            _driver.Close();
         }
-        
+
     }
 }
