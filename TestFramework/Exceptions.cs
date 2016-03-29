3372d6a63018b6aea60506dca3a9373adcbf8faf
rrrzzz <kalandrill@yahoo.com>
2016-03-29 16:40:41 +0300

    Remove ignored files

---
+++ TestFramework/Exceptions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestFramework
+{  
+    [Serializable]
+    public class IncorrectAuthorException: Exception
+    {
+        public IncorrectAuthorException() { }
+        public IncorrectAuthorException(string message) : base(message) { }
+        public IncorrectAuthorException(string message, Exception inner) : base(message, inner) { }
+        protected IncorrectAuthorException(
+            System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context) : base(info, context)
+        { }
+    }
+
+    
+}
