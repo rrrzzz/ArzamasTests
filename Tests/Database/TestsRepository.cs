49885c2ca43aebb5cc62ccedc3bc20f64d1795b4
rrrzzz <kalandrill@yahoo.com>
2016-04-08 23:35:54 +0300

    Added Database folder with repository interface and repository class with method to connect to local database and store test run statistics there

---
+++ Tests/Database/TestsRepository.cs
@@ -0,0 +1,53 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using Dapper;
+
+namespace Tests.Database
+{
+    public class TestsRepository : ITestStatsRepository
+    {
+       public void Add(TestSuiteRun run)
+        {
+            const string address = @"Server=АРИНА-VAIO\SQLEXPRESS;Initial Catalog=TestStats;Integrated Security=True";
+            using (IDbConnection connection = new SqlConnection(address))
+            {
+                const string query =
+                    @"INSERT INTO dbo.TestRuns (StartTime, RunningTime, Passed, Failed, Uncompleted, TotalNumberOfTests)
+                                               VALUES (@StartTime, @RunningTime, @Passed, @Failed, @Uncompleted, @TotalNumberOfTests);
+                                               SELECT CAST(SCOPE_IDENTITY() as int)";
+
+                var id = connection.Query<int>(query, new
+                {
+                    StartTime = run.StarTimeUtc,
+                    RunningTime = run.TotalTime,
+                    run.Passed,
+                    run.Failed,
+                    run.Uncompleted,
+                    TotalNumberOfTests = run.TotalTests
+                }).Single();
+
+                foreach (var test in run.Executions)
+                {
+                    InsertTest(test, id, connection);
+                }
+            }
+        }
+
+        public void InsertTest(TestExecution test, int id, IDbConnection connection)
+        {
+            const string query =
+                @"INSERT INTO dbo.TestExecutions (RunID, Name, Fixture, Result, Description, ExecutionTime)
+                                               VALUES (@RunID, @Name, @Fixture, @Result, @Description, @ExecutionTime)";
+            connection.Execute(query, new
+            {
+                RunID = id,
+                test.Name,
+                test.Fixture,
+                test.Result,
+                Description = test.Comment,
+                test.ExecutionTime
+            });
+        }
+    }
+}
--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -19,135 +19,130 @@ namespace Tests
     [TestFixture]
     public class BrowserTests
     {
-        //[SetUp]
-        //public void OpenBrowser()
-        //{
-        //    Browser.Start();
-        //}
-
-        //[TearDown]
-        //public void CloseBrowser()
-        //{
-        //    Browser.Quit();
-        //}
-
-        //[Test]
-        //public void CheckHomePageLinkAndTitle()
-        //{
-        //    Pages.HomePage.GoTo();
-        //    Assert.IsTrue(Pages.HomePage.IsAt());
-        //}
-
-        [Test]
-        public void Kill()
+        [SetUp]
+        public void OpenBrowser()
         {
-            Assert.AreEqual(0, 23);
+            Browser.Start();
         }
 
-        //[Test]
-        //public void CheckAuthorPage()
-        //{
-        //    Pages.HomePage.GoTo();
-        //    Pages.Footer.AuthorsClick();
-        //    Pages.AuthorsPage.ClickAnyAuthor();
-        //    Assert.IsTrue(Pages.Author.IsAt());
-        //}
+        [TearDown]
+        public void CloseBrowser()
+        {
+            Browser.Quit();
+        }
 
-        //[Test]
-        //public void CheckAllAuthors()
-        //{
-        //    Pages.AuthorsPage.GoTo();
-        //    Assert.IsTrue(Pages.AuthorsPage.CheckAllAuthors());
-        //}
+        [Test]
+        public void CheckHomePageLinkAndTitle()
+        {
+            Pages.HomePage.GoTo();
+            Assert.IsTrue(Pages.HomePage.IsAt());
+        }
 
-        //[Test]
-        //public void CheckAboutPage()
-        //{
-        //    Pages.HomePage.GoTo();
-        //    Pages.Footer.AboutClick();
-        //    Assert.IsTrue(Pages.AboutPage.IsAt());
-        //}
 
-        //[Test]
-        //public void CheckLicensePage()
-        //{
-        //    Pages.HomePage.GoTo();
-        //    Pages.Footer.LicenseClick();
-        //    Assert.IsTrue(Pages.LicensePage.IsAt());
-        //}
+        [Test]
+        public void CheckAuthorPage()
+        {
+            Pages.HomePage.GoTo();
+            Pages.Footer.AuthorsClick();
+            Pages.AuthorsPage.ClickAnyAuthor();
+            Assert.IsTrue(Pages.Author.IsAt());
+        }
 
-        //[Test]
-        //public void CheckHowToPage()
-        //{
-        //    Pages.AboutPage.GoTo();
-        //    Pages.AboutPage.HowToClick();
-        //    Assert.IsTrue(Pages.HowToPage.IsAt());
-        //}
+        [Test]
+        public void CheckAllAuthors()
+        {
+            Pages.AuthorsPage.GoTo();
+            Assert.IsTrue(Pages.AuthorsPage.CheckAllAuthors());
+        }
 
-        //[Test]
-        //public void CheckTeamPage()
-        //{
-        //    Pages.AboutPage.GoTo();
-        //    Pages.AboutPage.TeamClick();
-        //    Assert.IsTrue(Pages.TeamPage.IsAt());
-        //}
+        [Test]
+        public void CheckAboutPage()
+        {
+            Pages.HomePage.GoTo();
+            Pages.Footer.AboutClick();
+            Assert.IsTrue(Pages.AboutPage.IsAt());
+        }
 
-        //[Test]
-        //public void CheckContactPage()
-        //{
-        //    Pages.AboutPage.GoTo();
-        //    Pages.AboutPage.ContactClick();
-        //    Assert.IsTrue(Pages.ContactPage.IsAt());
-        //}
+        [Test]
+        public void CheckLicensePage()
+        {
+            Pages.HomePage.GoTo();
+            Pages.Footer.LicenseClick();
+            Assert.IsTrue(Pages.LicensePage.IsAt());
+        }
 
-        //[Test]
-        //public void CheckContactFormSubmit()
-        //{
-        //    Pages.ContactPage.GoTo();
-        //    Pages.ContactPage.FillForm();
-        //    Pages.ContactPage.Submit();
-        //    Assert.IsTrue(Pages.ContactPage.IsFormSubmitted());
-        //}
+        [Test]
+        public void CheckHowToPage()
+        {
+            Pages.AboutPage.GoTo();
+            Pages.AboutPage.HowToClick();
+            Assert.IsTrue(Pages.HowToPage.IsAt());
+        }
 
-        //[Test]
-        //public void SubscriptionInvalidEmail()
-        //{
-        //    Pages.HomePage.GoTo();
-        //    Pages.Footer.FillEmailForm();
-        //    Pages.Footer.SubmitEmailForm();
-        //    Assert.IsTrue(Pages.Footer.IsErrorMessageShown());
-        //}
+        [Test]
+        public void CheckTeamPage()
+        {
+            Pages.AboutPage.GoTo();
+            Pages.AboutPage.TeamClick();
+            Assert.IsTrue(Pages.TeamPage.IsAt());
+        }
 
-        //[Test]
-        //public void SubscriptionValidEmail()
-        //{
-        //    Pages.HomePage.GoTo();
-        //    Pages.Footer.FillEmailForm("kalandrill@yahoo.com");
-        //    Pages.Footer.SubmitEmailForm();
-        //    Assert.IsTrue(Pages.Footer.IsEmailSubscriptionSuccessful());
-        //}
+        [Test]
+        public void CheckContactPage()
+        {
+            Pages.AboutPage.GoTo();
+            Pages.AboutPage.ContactClick();
+            Assert.IsTrue(Pages.ContactPage.IsAt());
+        }
+
+        [Test]
+        public void CheckContactFormSubmit()
+        {
+            Pages.ContactPage.GoTo();
+            Pages.ContactPage.FillForm();
+            Pages.ContactPage.Submit();
+            Assert.IsTrue(Pages.ContactPage.IsFormSubmitted());
+        }
+
+        [Test]
+        public void SubscriptionInvalidEmail()
+        {
+            Pages.HomePage.GoTo();
+            Pages.Footer.FillEmailForm();
+            Pages.Footer.SubmitEmailForm();
+            Assert.IsTrue(Pages.Footer.IsErrorMessageShown());
+        }
+
+        [Test]
+        public void SubscriptionValidEmail()
+        {
+            Pages.HomePage.GoTo();
+            Pages.Footer.FillEmailForm("kalandrill@yahoo.com");
+            Pages.Footer.SubmitEmailForm();
+            Assert.IsTrue(Pages.Footer.IsEmailSubscriptionSuccessful());
+        }
     }
 
-    //[TestFixture]
-    //public class ApiTests
-    //{
+    [TestFixture]
+    public class ApiTests
+    {
 
-    //    [Test]
-    //    public void ApiValidEmailSubscription()
-    //    {
-    //        Assert.IsTrue(Api.IsEmailSubscriptionSuccessful());
-    //    }
+        [Test]
+        public void ApiValidEmailSubscription()
+        {
+            Assert.IsTrue(Api.IsEmailSubscriptionSuccessful());
+        }
 
-    //    [Test]
-    //    public void ApiInvalidEmailSubscription()
-    //    {
-    //        Assert.IsFalse(Api.IsEmailSubscriptionSuccessful("asdasd@asd"));
-    //    }
+        [Test]
+        public void ApiInvalidEmailSubscription()
+        {
+            Assert.IsFalse(Api.IsEmailSubscriptionSuccessful("asdasd@asd"));
+        }
 
-    //    [Test]
-    //    public void ApiInvalidFeedback()
-    //    {
-    //        Assert.IsTrue(Api.IsFeedbackError());
-    //    }
-    //}
+        [Test]
+        public void ApiInvalidFeedback()
+        {
+            Assert.IsTrue(Api.IsFeedbackError());
+        }
+    }
 }
