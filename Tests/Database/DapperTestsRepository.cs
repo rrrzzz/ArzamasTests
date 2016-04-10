df3b4412497cd7fa98165003e76ccb163cda2245
rrrzzz <kalandrill@yahoo.com>
2016-04-10 11:32:36 +0300

    Brought back connection through Dapper, added separate classes for Dapper and EF repositories

---
+++ Tests/Database/DapperTestsRepository.cs
@@ -0,0 +1,36 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using Dapper;
+
+namespace Tests.Database
+{
+    public class DapperTestsRepository : ITestStatsRepository
+    {
+       
+            public void Add(Run run)
+            {
+                using (IDbConnection connection = new SqlConnection(Constants.Address))
+                {
+                    const string query =
+                    @"INSERT INTO dbo.TestRuns (StartTime, RunningTime, Passed, Failed, Uncompleted, TotalNumberOfTests)
+                                                VALUES (@StartTime, @RunningTime, @Passed, @Failed, @Uncompleted, @TotalNumberOfTests);
+                                                SELECT CAST(SCOPE_IDENTITY() as int)";
+                    
+                    var id = connection.Query<int>(query, run).Single();
+                    foreach (var test in run.Executions)
+                    {
+                        InsertTest(test, id, connection);
+                    }
+                }
+            }
+
+        public void InsertTest(TestExecution test, int id, IDbConnection connection)
+        {
+            const string query =
+                @"INSERT INTO dbo.TestExecutions (RunID, Name, Fixture, Result, Description, ExecutionTime)
+                                              VALUES (@RunID, @Name, @Fixture, @Result, @Description, @ExecutionTime)";
+            connection.Execute(query, new {RunID = id, test.Name, test.Fixture, test.Result, test.Description, test.ExecutionTime});
+        }
+    }
+}
--- Tests/Database/TestStatsContext.cs
+++ Tests/Database/TestStatsContext.cs
@@ -4,19 +4,16 @@ namespace Tests.Database
 {
     public class TestStatsContext : DbContext
     {
-        private const string Address = @"Server=АРИНА-VAIO\SQLEXPRESS;Initial Catalog=TestStats;Integrated Security=True";
-
-        public TestStatsContext() : base(Address)
+        public TestStatsContext() : base(Constants.Address)
         {
         }
 
-        public DbSet<TestSuiteRun> TestRuns { get; set; }
+        public DbSet<Run> TestRuns { get; set; }
         public DbSet<TestExecution> TestExecutions { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<TestSuiteRun>().ToTable("TestRuns");
-            modelBuilder.Entity<TestExecution>().Property(x => x.TestSuiteRunID).HasColumnName("RunID");
+            modelBuilder.Entity<Run>().ToTable("TestRuns");
         }
     }
 }
